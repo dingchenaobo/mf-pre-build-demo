728ecbd2f8835b6326c0baad19d65c8b3c58c1fe
dingchen <dingchenaobo@163.com>
2022-04-13 22:52:26 +0800

    feat: blank tpl react 18, antd, ant-pro-layout.

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.less$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'less-loader',
+            options: {
+              lessOptions: {
+                javascriptEnabled: true,
+              },
+            }
+          }
+        ],
+      },
+      {
+        test: /\.css/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+            ]
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    port: 3000,
+  },
+  externals: {
+    'react': 'React',
+    'react-dom': 'ReactDOM',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public/index.html'),
+    }),
+  ]
+};
